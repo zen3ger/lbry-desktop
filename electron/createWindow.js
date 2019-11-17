161713f7a1efc9e3d9c38e092ce658727a53f6a7
Jessop <jessop@lbry.com>
2019-11-17 15:18:48 -0800

    test on windows

--- electron/createWindow.js
+++ electron/createWindow.js
@@ -45,9 +45,15 @@ export default appState => {
 
   let window = new BrowserWindow(windowConfiguration);
 
+  // if ( startMinimized ) {
+  //   console.log('START MINIMIZED')
+  //   window.minimize();
+  //   window.hide();
+  // }
   // Let us register listeners on the window, so we can update the state
   // automatically (the listeners will be removed when the window is closed)
   // and restore the maximized or full screen state.
+
   windowState.manage(window);
 
   let deepLinkingURI;
@@ -128,8 +134,14 @@ export default appState => {
   });
 
   window.once('ready-to-show', () => {
+    console.log('ready-to-show')
     startMinimized ? window.hide() : window.show();
   });
+
+  window.webContents.once('dom-ready', () => {
+    console.log('dom-ready')
+    startMinimized && window.hide()
+  })
 
   window.webContents.on('did-finish-load', () => {
     window.webContents.session.setUserAgent(`LBRY/${app.getVersion()}`);
